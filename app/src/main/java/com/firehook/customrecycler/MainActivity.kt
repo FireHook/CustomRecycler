885dfe0cdee57d308375eb213b292eb1c62a8f6d
Vladyslav Bondar <wladyslaw.bondar@gmail.com>
2024-07-25 10:43:57 +0200

    Initial commit

---
+++ app/src/main/java/com/firehook/customrecycler/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.firehook.customrecycler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
