f7d2df63aec060b4d01bbb5d5e5801a7e540605a
Vladyslav Bondar <wladyslaw.bondar@gmail.com>
2024-07-29 17:54:44 +0200

    Implemented simple horizontal recyclers

--- app/src/main/java/com/firehook/customrecycler/MainActivity.kt
+++ app/src/main/java/com/firehook/customrecycler/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.firehook.customrecycler
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = (0..17).map {
+            Color.argb(
+                255,
+                Random.nextInt(256),
+                Random.nextInt(256),
+                Random.nextInt(256)
+            )
+        }
+
+        findViewById<RecyclerView>(R.id.first_rv).apply {
+            layoutManager = LinearLayoutManager(
+                this@MainActivity,
+                RecyclerView.HORIZONTAL,
+                false
+            )
+            adapter = CustomAdapter(items)
+        }
+
+
+        findViewById<RecyclerView>(R.id.second_rv).apply {
+            layoutManager = LinearLayoutManager(
+                this@MainActivity,
+                RecyclerView.HORIZONTAL,
+                false
+            )
+            adapter = CustomAdapter(items.toMutableList())
+        }
+
     }
 }
